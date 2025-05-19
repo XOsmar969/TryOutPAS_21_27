17f37fc29d9e0e6ab5bcab98d8cf33127d443787
XOsmar969304 <osmarbaba234@gmail.com>
2025-05-19 10:52:16 +0700

    test

--- app/src/main/java/com/example/tryoutpas_21_27/DashboardFragment.java
+++ app/src/main/java/com/example/tryoutpas_21_27/DashboardFragment.java
@@ -1,4 +1,16 @@
 package com.example.tryoutpas_21_27;
 
-public class DashboardFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return view;
+    }
 }
--- app/src/main/java/com/example/tryoutpas_21_27/HomeFragment.java
+++ app/src/main/java/com/example/tryoutpas_21_27/HomeFragment.java
@@ -1,4 +1,16 @@
 package com.example.tryoutpas_21_27;
 
-public class HomeFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
 }
